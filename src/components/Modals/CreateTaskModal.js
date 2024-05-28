5d1d323626a472e25a68e806ac435780c6efec1b
parth0595 <parth0595@gmail.com>
2024-05-28 16:29:00 +0530

    Update CreateTaskModal.js

--- src/components/Modals/CreateTaskModal.js
+++ src/components/Modals/CreateTaskModal.js
@@ -6,6 +6,8 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTasks } from '../../redux/actions'; 
 
 export default function FormDialog({ openCreateTask, handleCloseTask }) {
 
@@ -14,14 +16,30 @@ export default function FormDialog({ openCreateTask, handleCloseTask }) {
     const [priority, setPriority] = useState('');
     const [date, setDate] = useState('');
     const inputRef = useRef();
+    const dispatch = useDispatch();
+    const stateData = useSelector((state)=>state)
+    console.log(stateData);
 
+    // const handleSubmitTask = (event) => {
+    //     event.preventDefault();
+    //     // const id =  JSON.parse(sessionStorage.getItem('userDetails', 'userDetails.users.id'));
+    //     const taskDetails = {taskName : taskName , priority: priority, stage:stage , date : date };
+    //     const prevtaskDetails = JSON.parse(sessionStorage.getItem('taskDetails'))|| [];
+    //     const updatedTaskDetails = [...prevtaskDetails , taskDetails]
+    //     sessionStorage.setItem('tasks' , JSON.stringify(updatedTaskDetails));
+    //     handleCloseTask();
+    // }
     const handleSubmitTask = (event) => {
         event.preventDefault();
-        // const id =  JSON.parse(sessionStorage.getItem('userDetails', 'userDetails.users.id'));
         const taskDetails = {taskName : taskName , priority: priority, stage:stage , date : date };
-        const prevtaskDetails = JSON.parse(sessionStorage.getItem('taskDetails'))|| [];
+        // added below
+        const userName = sessionStorage.getItem('userName')
+        const prevtaskDetails = JSON.parse(sessionStorage.getItem(userName)) || [];
+        console.log(prevtaskDetails);
         const updatedTaskDetails = [...prevtaskDetails , taskDetails]
-        sessionStorage.setItem('tasks' , JSON.stringify(updatedTaskDetails));
+        sessionStorage.setItem(userName , JSON.stringify(updatedTaskDetails));
+        dispatch(setTasks(updatedTaskDetails))
+        // added above
         handleCloseTask();
     }
 
