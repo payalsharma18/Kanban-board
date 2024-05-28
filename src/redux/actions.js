3df71444c7cb2a4f99944d12f2dba951327aa635
parth0595 <parth0595@gmail.com>
2024-05-28 16:27:18 +0530

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,11 @@
 export const REGISTER_USER = 'REGISTER_USER';
+export const SET_TASKS = 'SET_TASKS';
 
 export const registerUser = (userDetails)=>({
     type : REGISTER_USER,
     payload : userDetails
+});
+export const setTasks = (allTasks)=>({
+    type : SET_TASKS,
+    payload : allTasks
 });
